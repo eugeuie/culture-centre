b666101d76ff67bea575bed65e2ad4fe376f337a
Evgenia Abdullaeva <evabdullaeva@gmail.com>
2022-09-11 22:36:27 +0300

    done

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "events"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("category/<int:category_id>/", views.category, name="category"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Event
+from .models import Event, Category
 
 
 class IndexView(generic.ListView):
@@ -9,3 +9,27 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Event.objects.order_by("time")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+
+
+class DetailView(generic.DetailView):
+    model = Event
+    template_name = "events/detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["participants"] = self.object.participants.all()
+        return context
+
+
+def category(request, category_id):
+    events = Event.objects.filter(category__pk=category_id)
+    context = {
+        "events": events,
+        "categories": Category.objects.all(),
+    }
+    return render(request, "events/index.html", context)
